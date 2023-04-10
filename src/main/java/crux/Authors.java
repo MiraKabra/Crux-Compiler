c88a4fc789f78d0e43c39f674a890020a0660c78
Mira Kabra <mirakabra123@gmail.com>
2023-04-10 08:55:53 -0700

    Author and gitignore

--- src/main/java/crux/Authors.java
+++ src/main/java/crux/Authors.java
@@ -2,7 +2,7 @@ package crux;
 
 final class Authors {
   // TODO: Add author information.
-  static final Author[] all = {new Author("student name", "student id", "UCInetID"),};
+  static final Author[] all = {new Author("Mira Kabra", "69963587", "mkabra1"),};
 }
 
 
