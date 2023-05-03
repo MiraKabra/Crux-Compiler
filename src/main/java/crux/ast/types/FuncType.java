6b094e272466257804a6f44fcb3fc58f58741525
Mira Kabra <mirakabra123@gmail.com>
2023-05-03 14:26:26 -0700

    progress till now

--- src/main/java/crux/ast/types/FuncType.java
+++ src/main/java/crux/ast/types/FuncType.java
@@ -1,5 +1,7 @@
 package crux.ast.types;
 
+import java.util.Iterator;
+
 /**
  * The field args is a TypeList with a type for each param. The type ret is the type of the function
  * return. The function return could be int, bool, or void. This class should implement the call
@@ -27,5 +29,20 @@ public final class FuncType extends Type implements java.io.Serializable {
   @Override
   public String toString() {
     return "func(" + args + "):" + ret;
+  }
+
+  @Override
+  Type call(Type args){
+    if(this.args.equivalent(args)) return ret;
+    return super.call(args);
+  }
+
+  @Override
+  public boolean equivalent(Type that) {
+    if(!(that instanceof FuncType)) return false;
+    FuncType given = (FuncType) that;
+    if(!this.ret.equivalent(given.ret)) return false;
+    if(!this.args.equivalent(given.args)) return false;
+    return true;
   }
 }
--- src/main/java/crux/ast/types/IntType.java
+++ src/main/java/crux/ast/types/IntType.java
@@ -11,4 +11,46 @@ public final class IntType extends Type implements java.io.Serializable {
   public String toString() {
     return "int";
   }
+
+  @Override
+  Type add(Type that){
+    if(that instanceof IntType) return new IntType();
+    else{
+      return super.add(that);
+    }
+  }
+  @Override
+  Type sub(Type that){
+    if(that instanceof IntType) return new IntType();
+    else{
+      return super.sub(that);
+    }
+  }
+  @Override
+  Type mul(Type that){
+    if(that instanceof IntType) return new IntType();
+    else{
+      return super.mul(that);
+    }
+  }
+  @Override
+  Type div(Type that){
+    if(that instanceof IntType) return new IntType();
+    else{
+      return super.div(that);
+    }
+  }
+  @Override
+  Type compare(Type that){
+    if(that instanceof IntType) return new BoolType();
+    else{
+      return super.compare(that);
+    }
+  }
+
+  @Override
+  public boolean equivalent(Type that) {
+    if(that instanceof IntType) return true;
+    return false;
+  }
 }
