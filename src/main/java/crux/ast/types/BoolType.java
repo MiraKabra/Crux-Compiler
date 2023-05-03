293293ed888c748dc7aaa4473cbde4906505fe84
Mira Kabra <mirakabra123@gmail.com>
2023-05-03 14:26:26 -0700

    progress till now

--- src/main/java/crux/ast/types/BoolType.java
+++ src/main/java/crux/ast/types/BoolType.java
@@ -11,4 +11,37 @@ public final class BoolType extends Type implements java.io.Serializable {
   public String toString() {
     return "bool";
   }
+
+  @Override
+  Type or(Type that){
+    if(that.equivalent(new BoolType())){
+      return new BoolType();
+    }
+    return super.or(that);
+  }
+
+  @Override
+  Type not(){
+    return new BoolType();
+  }
+  @Override
+  Type assign(Type that){
+    if(that.equivalent(new BoolType())){
+      return new BoolType();
+    }
+    return super.assign(that);
+  }
+  @Override
+  Type compare(Type that){
+    if(that.equivalent(new BoolType())){
+      return new BoolType();
+    }
+    return super.compare(that);
+  }
+
+  @Override
+  public boolean equivalent(Type that) {
+    if(that instanceof BoolType) return true;
+    return false;
+  }
 }
--- src/main/java/crux/ast/types/TypeChecker.java
+++ src/main/java/crux/ast/types/TypeChecker.java
@@ -6,6 +6,7 @@ import crux.ast.traversal.NullNodeVisitor;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -13,8 +14,13 @@ import java.util.stream.Stream;
  * This class will associate types with the AST nodes from Stage 2
  */
 public final class TypeChecker {
+  private Map<Node, Type> nodeTypeMap;
   private final ArrayList<String> errors = new ArrayList<>();
+  private Stack<Integer> root = new Stack<>();
 
+  public TypeChecker(){
+    nodeTypeMap = new HashMap<>();
+  }
   public ArrayList<String> getErrors() {
     return errors;
   }
@@ -57,12 +63,16 @@ public final class TypeChecker {
    */
   private final class TypeInferenceVisitor extends NullNodeVisitor<Void> {
     @Override
-    public Void visit(VarAccess vaccess) {
+    public Void visit(VarAccess varAccess) {
+      Symbol symbol = varAccess.getSymbol();
+      setNodeType(varAccess, symbol.getType());
       return null;
     }
 
     @Override
     public Void visit(ArrayDeclaration arrayDeclaration) {
+      Symbol symbol = arrayDeclaration.getSymbol();
+      setNodeType(arrayDeclaration, symbol.getType());
       return null;
     }
 
@@ -83,16 +93,44 @@ public final class TypeChecker {
 
     @Override
     public Void visit(DeclarationList declarationList) {
+      List<Node> declarations;
+      declarations = declarationList.getChildren();
+      for(Node node : declarations){
+        Declaration declaration = (Declaration) node;
+        declaration.accept(this);
+      }
       return null;
     }
 
     @Override
     public Void visit(FunctionDefinition functionDefinition) {
+      Symbol symbol = functionDefinition.getSymbol();
+      List<Symbol> parameters = functionDefinition.getParameters();
+      StatementList statementList = functionDefinition.getStatements();
+      FuncType currFuncType = (FuncType) symbol.getType();
+      boolean return_required = false;
+      if(!currFuncType.getRet().equivalent(new VoidType())){
+        return_required = true;
+      }
+      if(return_required){
+        root.push(0);
+      }
+      statementList.accept(this);
+      if(return_required && root.pop() > 0){
+        addTypeError(functionDefinition, "error: Not returning at all possible paths.");
+      }
       return null;
     }
 
     @Override
     public Void visit(IfElseBranch ifElseBranch) {
+      OpExpr cond = (OpExpr) ifElseBranch.getCondition();
+      Type type = ((BaseNode)cond.getLeft()).getType().compare(((BaseNode)cond.getRight()).getType());
+      if(!(new BoolType().equivalent(type))){
+        addTypeError(ifElseBranch, "the condition of if else should be of type boolean");
+      }
+      ifElseBranch.getThenBlock().accept(this);
+      ifElseBranch.getElseBlock().accept(this);
       return null;
     }
 
@@ -128,11 +166,31 @@ public final class TypeChecker {
 
     @Override
     public Void visit(StatementList statementList) {
+      root.push(0);
+      List<Node> list = statementList.getChildren();
+      boolean return_included = false;
+      for(Node node: list){
+        Statement statement = (Statement) node;
+        statement.accept(this);
+        if(is_return_type(statement)){
+          return_included = true;
+        }
+      }
+      root.pop();
+      if(!return_included){
+        int temp = root.pop();
+        root.push(temp+1);
+      }
       return null;
     }
-
+    private boolean is_return_type(Statement statement){
+      if(statement instanceof Return) return true;
+      return false;
+    }
     @Override
     public Void visit(VariableDeclaration variableDeclaration) {
+      Symbol symbol = variableDeclaration.getSymbol();
+      setNodeType(variableDeclaration, symbol.getType());
       return null;
     }
   }
